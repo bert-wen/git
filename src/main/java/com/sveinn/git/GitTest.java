dd336c1f3966dcb223a9a8a866c0306ac44bf787
sveinn <2046641767@qq.com>
2022-01-20 23:01:44 +0800

    first commit

---
+++ src/main/java/com/sveinn/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.sveinn.git;
+
+/**
+ * @description: TODO
+ * @title: GitTest
+ * @email: 2046641767@qq.com
+ * @projectName: git
+ * @author: Sveinn
+ * @date: 2022/1/20 22:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
